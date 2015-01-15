6b92d0a35f6a73252ac63b76b8674e4851c4f16b
Joe Borg <joe@josephb.org>
2015-01-15 21:14:35 +0000

    Adding setup Adding interface

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Vampire',
+    version='0.1',
+    packages=['vmp'],
+    scripts=['vmp'],
+    url='https://josephb.org/projects/vmp/',
+    license='GPLv2',
+    author='Joseph Borg',
+    author_email='joe@josephb.org',
+    description='Deploy Python'
+)
--- vampire/build.py
+++ vampire/build.py
@@ -1,7 +1,9 @@
 import os
-import logger
+import logging
 import urllib2
 import subprocess
+
+logger = logging.getLogger('vampire')
 
 
 class PythonBuild(object):
@@ -9,17 +11,23 @@ class PythonBuild(object):
     Class abstracting a Python 
     build.
     """
-    def __init__(self, version, target=os.path.join(os.path.expanduser('~'), 'python'), host='localhost'):
+    def __init__(self, version, target, host):
         """
+        Set the constants.
         """
+        logger.debug('Version: %s' % version)
+        logger.debug('Target: %s' % target)
+        logger.debug('Host: %s' % host)
         self.version = version
         self.target = target
+        self.host = host
         self.package_url = 'https://www.python.org/ftp/python/%s/Python-%s.tgz' % (self.version, self.version)
-        self.tempoary_directory = '/tmp'
-        self.package_path = os.path.join(self.tempoary_directory, os.path.basename(self.package_url))
+        self.temporary_directory = '/tmp'
+        self.package_path = os.path.join(self.temporary_directory, os.path.basename(self.package_url))
 
     def getPackage(self):
         """
+        Download the python build.
         """
         download = urllib2.urlopen(self.package_url)
         with open(self.package_path,'wb') as f:
@@ -27,14 +35,16 @@ class PythonBuild(object):
 
     def extractPackage(self):
         """
+        Extract the build.
         """
         with open(self.package_path) as f:
             pass  # TODO: Extract package.
 
     def compilePackage(self):
         """
+        Compile the build.
         """
-        configure_proc = suprocess.Popen(['./configure', '--prefix=%s' % self.target])
+        configure_proc = subprocess.Popen(['./configure', '--prefix=%s' % self.target])
         configure_proc.wait()
         if configure_proc.returncode != 0:
             configure_proc.communicate()
